c3e704286adf33ecc76e97596dc7724bda1023d8
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-06-27 15:43:52 +0000

    Starting configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const clientPath = path.join(__dirname, 'client/');
+const publicPath = path.join(__dirname, 'server/public/');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: clientPath,
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [{
+      test: /\.jsx/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            '@babel/plugin-transform-react-jsx'
+          ]
+        }
+      }
+    }]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    contentBase: publicPath,
+    historyApiFallback: true,
+    watchContentBase: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000'
+      }
+    }
+  }
+};
