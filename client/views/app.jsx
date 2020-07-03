87f463e81d3171dbb6aa3b328a40e04306ccf448
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-07-03 15:17:50 -0700

    Initial component layout setup

--- client/views/app.jsx
+++ client/views/app.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Welcome to Redstar Component Library</h1>;
+  return (
+    <div className="section-wrapper">
+      <header className="header">
+        <h1>Redstar Component Library</h1>
+      </header>
+      <div className="content-container">
+        <div className="side-scroll">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+        <div className="main">
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
