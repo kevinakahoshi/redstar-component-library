86737f18660d8c91dc777e2743e6d6ca4567352b
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-07-10 15:17:58 -0700

    Updated Tooltip and some layout

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Accordion from '../components/elements/accordion';
+import TooltipWrapper from './elements/tooltip-wrapper';
 
 const Main = () => {
   return (
     <div className="main">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptatibus fugiat officia illo non consectetur debitis fuga culpa, exercitationem modi, optio iure perspiciatis facilis beatae placeat ipsa possimus id? Consequatur.</p>
-      <p>Veniam assumenda libero, provident tempore nulla voluptatem iste expedita quasi voluptate aliquam ipsa magnam labore soluta similique at pariatur exercitationem quia cum ducimus, debitis eos nihil impedit. Illum, accusamus repellendus.</p>
-      <p>Distinctio magnam non ipsa itaque voluptates, sed, dolores, reprehenderit culpa aperiam iure nisi a velit! Culpa doloribus voluptatem cum. Ipsam dolorem quo voluptatum, a consequatur labore hic ut officia adipisci?</p>
-      <p>Dolores nisi doloribus pariatur eos repudiandae exercitationem error nobis magnam esse cupiditate porro, laudantium, libero voluptatibus earum obcaecati sequi nihil non ipsa sed labore. Consectetur quia vero reiciendis iusto nihil!</p>
-      <p>Rerum numquam nam tempore id nisi, fuga dolorum facilis sequi unde quibusdam saepe enim quo nostrum beatae cumque mollitia voluptatum magnam quasi consectetur molestiae recusandae nobis architecto! Iure, voluptas ex!</p>
+      <Accordion
+        buttonText="Hello Please"
+        innerText="Hello Hello Hello" />
+      <TooltipWrapper
+        message="Here is that new Tooltip Wrapper you were hearing about!">
+        <p>This <pre>p</pre> element is wrapped in the <pre>TooltipWrapper</pre></p>
+      </TooltipWrapper>
     </div>
   );
 };
