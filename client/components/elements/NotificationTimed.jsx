62b7b9abaedfc64717bcfee9c961398eb98d820d
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-07-29 19:48:55 -0700

    Saving progress for the timed notification component

--- client/components/elements/NotificationTimed.jsx
+++ client/components/elements/NotificationTimed.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, {
+// useEffect
+} from 'react';
 
-const NotificationPersistent = ({ maxLength, notificationText, setShow, show }) => {
-  if (notificationText.length > maxLength && maxLength > 0 && typeof maxLength === 'number') {
+const NotificationTimed = ({
+  duration,
+  maxLength,
+  notificationText,
+  setShowTimed,
+  show
+}) => {
+  if (
+    notificationText.length > maxLength &&
+    maxLength > 0 &&
+    typeof maxLength === 'number'
+  ) {
     notificationText = notificationText.slice(0, maxLength - 1);
   }
 
   if (!notificationText || typeof notificationText !== 'string') return null;
 
+  if (!show) {
+    setTimeout(() => {
+      setShowTimed(true);
+    }, duration);
+  }
+
   return (
-    <div
-      className={`notification ${show ? 'hide' : 'show'}`}>
+    <div className={`notification ${show ? 'hide' : 'show'}`}>
       <div className="notification-text-wrapper">
-        <p>
-          { notificationText }
-        </p>
+        <p>{notificationText}</p>
       </div>
       <div className="notification-button-wrapper">
         <button
           className="close-notification"
-          onClick={() => setShow(true)}>
+          onClick={() => setShowTimed(true)}
+        >
           Close
         </button>
+      </div>
+      <div className="timer-wrapper">
+        <div
+          className="timer-inner"
+          style={{
+            width: `${show ? 0 : 100}%`,
+            transition: `${duration}ms width linear`
+          }}
+        />
       </div>
     </div>
   );
 };
 
-export default NotificationPersistent;
+export default NotificationTimed;
